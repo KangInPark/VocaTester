175b250837081dc3b16b993de0ae1603a0c584dd
KangInPark <pkihome@naver.com>
2022-01-29 16:19:51 +0900

    기능 추가: 오늘의 단어 옵션창 추가

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 main_ui = uic.loadUiType("MainWindow.ui")[0]
 daily_ui = uic.loadUiType("DailyWindow.ui")[0]
 total_ui = uic.loadUiType("TotalWindow.ui")[0]
+dailyop_ui = uic.loadUiType("DailyOption.ui")[0]
 
 class MainWindow(QMainWindow, main_ui):
     def __init__(self):
@@ -15,19 +16,37 @@ class MainWindow(QMainWindow, main_ui):
         self.btn2.clicked.connect(self.TotalBtn)
     
     def DailyBtn(self):
-        self.dailyWindow = DailyWindow()
+        self.dailyOption = DailyOption()
         
     def TotalBtn(self):
         self.totalWindow = TotalWindow()
 
-class DailyWindow(QDialog, daily_ui):
-    def __init__(self, ):
+class DailyOption(QDialog, dailyop_ui):
+    def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.show()
+        self.btn.clicked.connect(self.Btn)
+    
+    def Btn(self):
+        plist = []
+        if self.chk2.isChecked():
+            plist.append(1)
+        if self.chk3.isChecked():
+            plist.append(2)
+        self.close()
+        self.dailyWindow = DailyWindow(self.chk1.isChecked(), plist)
+
+class DailyWindow(QDialog, daily_ui):
+    def __init__(self, rnd, plist):
+        super().__init__()
+        self.setupUi(self)
+        self.rnd = rnd
+        self.plist = plist
+        self.show()
     
 class TotalWindow(QDialog, total_ui):
-    def __init__(self, ):
+    def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.show()
