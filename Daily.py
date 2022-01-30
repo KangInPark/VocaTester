3ebf3305703bb157befe1f532d72b14768906060
KangInPark <pkihome@naver.com>
2022-01-30 18:40:55 +0900

    기능추가: 성능 개선을 위해 단어 list 객체를 파일로 저장
    pickle 모듈을 활용하여 .pkl 확장자로 data폴더에 저장함.

--- Daily.py
+++ Daily.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 import datetime
 import os
+import pickle
 from random import choice, randint
 import sys
 from openpyxl import Workbook, load_workbook
@@ -13,19 +14,23 @@ class Daily():
         self.wb = load_workbook(fdir)
         self.ws = self.wb.active
         self.setWord()
-    
+        self.warn = 0
+        if 2 in plist or 3 in plist:
+            with open(os.getcwd() + f'\\data\\{str(datetime.date.today())}.pkl', 'rb') as f:
+                self.mlist = pickle.load(f)
+            if len(self.mlist) < 6:
+                self.word = []
+                self.warn = 1
     def setWord(self):
         self.word = []
-        self.mlist = []
         for row in self.ws.iter_rows():
             self.word.append((row[0].value, row[1].value, row[2].value))
-            self.mlist.append(row[2].value)
-        if len(self.word) <= 4 and 2 in self.plist:
-            self.plist.remove(2)
         self.accumulate(self.word)
     
     def nextQ(self):
         if not self.word:
+            if self.warn == 1:
+                return -1, None
             return 0, None
         if self.rnd:
             idx = randint(0,len(self.word)-1)
@@ -39,7 +44,7 @@ class Daily():
         elif n == 2:
             ret.append(tmp)
             while len(ret) < 4:
-                sam = choice(self.mlist)
+                sam = choice(self.mlist)[2]
                 if sam != ret[0][2] and sam not in ret:
                     ret.append(sam)
         return n, ret
@@ -92,3 +97,11 @@ class Daily():
                 ws.cell(i,2).value = word[i-1][1]
             ws.cell(i,3).value = word[i-1][2]
         wb.save(path)
+        pick = []
+        idx = wb.sheetnames.index(today) 
+        if idx > 0:
+            with open(os.getcwd() + f'\\data\\{wb.sheetnames[idx-1]}.pkl', 'rb') as f:
+                pick = pickle.load(f)
+        pick.extend(word)
+        with open(os.getcwd() + f'\\data\\{today}.pkl', 'wb') as f:
+            pickle.dump(pick, f)
